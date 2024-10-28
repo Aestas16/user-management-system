f4478883cc205c12d7a9dcd7cc9a76c4e5ba0d58
Aestas16 <Aestas16@qq.com>
2024-10-28 22:41:23 +0800

    feat: 增加读取配置文件功能

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
     "net/http"
     "github.com/labstack/echo"
+
+    "user-management-system/internal/config"
 )
 
 func main() {
---
+++ internal/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+    "os"
+    "gopkg.in/yaml.v3"
+)
+
+type Configuration struct {
+    Server  ServerConfig    `yaml:"server"`
+}
+type ServerConfig struct {
+    Port    string      `yaml:"port"`
+    Admin   AdminConfig `yaml:"admin"`
+}
+type AdminConfig struct {
+    Username    string  `yaml:"username"`
+    Password    string  `yaml:"password"`
+}
+
+var Config Configuration
+
+func Init() {
+    yamlFile, err := os.ReadFile("config.yaml")
+    if err != nil {
+        panic(err)
+    }
+    err = yaml.Unmarshal(yamlFile, &Config)
+    if err != nil {
+        panic(err)
+    }
+}
