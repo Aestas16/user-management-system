fefbba4124f44265f672c74c4ce3055802e04372
Aestas16 <Aestas16@qq.com>
2024-10-31 23:33:03 +0800

    feat(router): 完成初始化路由功能

--- internal/router/router.go
+++ internal/router/router.go
@@ -6,10 +6,19 @@ import (
 
     "user-management-system/internal/config"
     "user-management-system/internal/controller"
+    "user-management-system/internal/utils"
 )
 
-func InitRouter() {
+func InitRouter(e *echo.Echo) {
     apiVersion := config.Config.Server.Version
-    e.GET(fmt.Sprintf("/%s/user/:id", apiVersion), controller.userInfo)
-    e.POST(fmt.Sprintf("/%s/user/:id", apiVersion), controller.userInfo)
+    apiUser := e.Group(fmt.Sprintf("/%s/user", apiVersion))
+    apiUser.Use(utils.JWTAuthMiddleware())
+    apiUser.GET("/:id", controller.userInfo)
+    apiUser.POST("/:id", controller.userInfo)
+    apiUser.POST("/:id/update", controller.updateUser)
+    apiUser.POST("/:id/delete", controller.deleteUser)
+    apiUser.POST("/token", controller.refreshToken)
+    api := e.Group(fmt.Sprintf("/%s", apiVersion))
+    api.POST("/login", controller.loginUser)
+    api.POST("/register", controller.registerUser)
 }
