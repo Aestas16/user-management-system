7bc70095836145f97405100d2ce38f44195ea8c3
Aestas16 <Aestas16@qq.com>
2024-10-31 23:22:39 +0800

    feat(controller): 增加用户登录功能

--- internal/controller/user.go
+++ internal/controller/user.go
@@ -20,8 +20,9 @@ type User struct {
 func userInfo(c echo.Context) error {
     claims := c.Get("claims")
     id := c.Param("id")
-    if !claims.isAdmin && claims.user.ID != id:
+    if !claims.isAdmin && claims.user.ID != id {
         return echo.NewHTTPError(403, "access denied")
+    }
     user, err := model.findUserById(id)
     if err == model.userNotFound {
         return echo.ErrNotFound
@@ -34,14 +35,15 @@ func userInfo(c echo.Context) error {
     }
     resp.username = user.username
     resp.email = user.email
-    return c.JSON(200, &resp);
+    return c.JSON(200, &resp)
 }
 
 func updateUser(c echo.Context) error {
     claims := c.Get("claims")
     id := c.Param("id")
-    if !claims.isAdmin && claims.user.ID != id:
+    if !claims.isAdmin && claims.user.ID != id {
         return echo.NewHTTPError(403, "access denied")
+    }
     user, err := model.findUserById(id)
     if err == model.userNotFound {
         return echo.ErrNotFound
@@ -53,7 +55,7 @@ func updateUser(c echo.Context) error {
         return echo.ErrBadRequest
     }
     user.Username = req.Username
-    user.Password = fmt.Sprintf("%x", md5.Sum([]byte(req.Password)));
+    user.Password = fmt.Sprintf("%x", md5.Sum([]byte(req.Password)))
     user.Email = req.Email
     if err := model.saveUser(user), err != nil {
         return echo.ErrInternalServerError
@@ -68,8 +70,9 @@ func updateUser(c echo.Context) error {
 func deleteUser(c echo.Context) error {
     claims := c.Get("claims")
     id := c.Param("id")
-    if !claims.isAdmin:
+    if !claims.isAdmin {
         return echo.NewHTTPError(403, "access denied")
+    }
     user, err := model.findUserById(id)
     if err == model.userNotFound {
         return echo.ErrNotFound
@@ -96,7 +99,7 @@ func registerUser(c echo.Context) error {
     }
     user := model.User{}
     user.Username = req.Username
-    user.Password = fmt.Sprintf("%x", md5.Sum([]byte(req.Password)));
+    user.Password = fmt.Sprintf("%x", md5.Sum([]byte(req.Password)))
     user.Email = req.Email
     err := model.createUser(user)
     if err == model.userAlreadyExist {
@@ -109,6 +112,46 @@ func registerUser(c echo.Context) error {
     }
     resp.message = "Success!"
     return c.JSON(201, &resp)
+}
+
+func loginUser(c echo.Context) error {
+    var req struct {
+        Username    string
+        Password    string
+    }
+    if err := c.Bind(&req), err != nil {
+        return echo.ErrBadRequest
+    }
+    if req.Username == config.Config.Server.Admin.Username && req.Password == config.Config.Server.Admin.Password {
+        tokenString, err := utils.generateToken(model.User{}, true)
+        if err != nil {
+            return echo.ErrInternalServerError
+        }
+        var resp struct {
+            tokenString string
+        }
+        resp.tokenString = tokenString
+        return c.JSON(200, &resp)
+    }
+    req.Password = fmt.Sprintf("%x", md5.Sum([]byte(req.Password)))
+    user, err := model.findUserByName(req.Username)
+    if err == model.userNotFound {
+        return echo.NewHTTPError(403, "user not found")
+    } else if err != nil {
+        return echo.ErrInternalServerError
+    }
+    if user.Password != req.Password {
+        return echo.NewHTTPError(403, "wrong password")
+    }
+    tokenString, err := utils.generateToken(user, false)
+    if err != nil {
+        return echo.ErrInternalServerError
+    }
+    var resp struct {
+        tokenString string
+    }
+    resp.tokenString = tokenString
+    return c.JSON(200, &resp)
 }
 
 func refreshToken(c echo.Context) error {
